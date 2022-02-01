576c8a81f08f4c2ad3f4df5e2ef062e33f78d02e
Craig <chisholm.craig@gmail.com>
2022-01-31 20:17:44 -0400

    Add functionality to local backend

--- backends/local/local.go
+++ backends/local/local.go
@@ -1,5 +1,11 @@
 package local
 
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
 var (
 	BUCKET_NAME     string
 	STATE_FILE_NAME string
@@ -7,13 +13,47 @@ var (
 
 type LocalBackend struct{}
 
+func init() {
+	var (
+		envExists bool
+	)
+	STATE_FILE_NAME, envExists = os.LookupEnv("CFSTATE_STATE_FILE_NAME")
+
+	if !envExists {
+		panic(fmt.Errorf("CFSTATE_STATE_FILE_NAME not set"))
+	}
+}
+
 func (lb LocalBackend) NewBackend() LocalBackend {
 	return LocalBackend{}
 }
 
 func (lb LocalBackend) UpdateState(stateFile []byte) (err error) {
+	var (
+		now                time.Time = time.Now()
+		sec                int64     = now.Unix()
+		stateFileName      string    = fmt.Sprintf("./%s.json", STATE_FILE_NAME)
+		savedStateFileName string    = fmt.Sprintf("./prev_states/state-%d.json", sec)
+	)
+
+	if err = os.Rename(stateFileName, savedStateFileName); err != nil {
+		return err
+	}
+
+	if err = os.WriteFile(stateFileName, stateFile, 0644); err != nil {
+		return err
+	}
+
+	return err
 
 }
 
 func (lb LocalBackend) GetState() (output []byte, err error) {
+	stateFileName := fmt.Sprintf("./%s.json", STATE_FILE_NAME)
+	output, err = os.ReadFile(stateFileName)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, err
 }
